d5da8caddc9c125e8b2d70ef9679caee08e6c5e1
Daniel Stepan <99193502+danielstepan@users.noreply.github.com>
2024-09-06 19:32:17 +0200

    Remove debugging output counting pods in cluster

--- pkg/webhook/fetch.go
+++ pkg/webhook/fetch.go
@@ -7,7 +7,6 @@ import (
 
 	config "github.com/danielstepan/k8s-init-injector/pkg/config"
 	apiv1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type InjectableInitContainerList struct {
@@ -22,14 +21,6 @@ type InjectableInitContainerList struct {
 func FetchInjectableInitContainers() InjectableInitContainerList {
 
 	clientSet := config.ClientSet
-
-	pods, err := clientSet.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	fmt.Println("There are %d pods in the cluster\n", len(pods.Items))
 
 	d, err := clientSet.RESTClient().Get().AbsPath("/apis/danielstepan.cz/v1/injectableinitcontainers").DoRaw(context.TODO())
 	if err != nil {
